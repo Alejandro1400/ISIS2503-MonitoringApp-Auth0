211c721173568bab89534e46178273393fe219c7
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2022-11-18 21:25:31 -0500

    Añadido boton de login

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 LOGIN_URL = "/login/auth0" 
 LOGIN_REDIRECT_URL = "/" 
+#LOGOUT_REDIRECT_URL = "https://syntax-error.auth0.com/v2/logout?returnTo=http%3A%2F%2F127.0.0.1:8000" 
 LOGOUT_REDIRECT_URL = "https://syntax-error.auth0.com/v2/logout?returnTo=http%3A%2F%2F34.133.95.185:8080" 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'syntax-error.auth0.com' 
